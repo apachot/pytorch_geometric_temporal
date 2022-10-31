a61ce192e508d34d35b39143d295055cc39d65ab
Arnault Pachot <apachot@openstudio.fr>
2022-10-31 13:50:31 +0100

    displaying predictions

--- lrgcn_example_on_DynamicProductspace.py
+++ lrgcn_example_on_DynamicProductspace.py
@@ -3,6 +3,11 @@ try:
 except ImportError:
     def tqdm(iterable):
         return iterable
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 import pandas as pd
 import torch
 import torch.nn.functional as F
@@ -56,11 +61,15 @@ for epoch in tqdm(range(200)):
 model.eval()
 cost = 0
 h, c = None, None
+y_list, y_hat_list = [], []
 for time, snapshot in enumerate(test_dataset):
     y_hat, h, c = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr, h, c)
     cost_tmp = torch.mean((torch.abs(y_hat.T-snapshot.y))**2)
     #cost_tmp = torch.mean((torch.abs(y_hat.T-snapshot.y)))
     cost = cost + cost_tmp
+    # Store for analysis below
+    y_list.append(snapshot.y.detach().cpu().numpy())
+    y_hat_list.append(y_hat.T.detach().cpu().numpy())
 cost = cost / (time+1)
 cost = cost.item()
 print("MSE: {:.10f}".format(cost))
@@ -85,3 +94,45 @@ line = [str(i[0]).zfill(6) for i in line]
 Exports.append(line)
 
 pd.DataFrame(Exports).to_csv('predict_exports.csv', header=False, index=False)
+print("y_hat_list", y_hat_list)
+print("y_list", y_list)
+
+
+'''
+product = 1
+
+
+
+preds = np.asarray([pred[0][product] for pred in y_hat_list])
+print("preds", preds)
+labs  = np.asarray([label[product] for label in y_list])
+print("Data points:,", preds.shape)
+
+plt.figure(figsize=(20,5))
+
+#plt.plot(hs_product_code)
+sns.lineplot(data=preds, label="pred")
+sns.lineplot(data=labs, label="true")
+plt.show()
+'''
+
+year = 10
+
+print("y_hat_list", y_hat_list[0][0])
+print("y_list", y_list[0])
+
+
+plt.figure(figsize=(20,5))
+
+#plt.plot(hs_product_code)
+sns.set_theme(style="whitegrid")
+product_list = pd.read_csv('./hs_product_code.csv', header=None, index_col=False)
+labels = [i.zfill(6) for i in product_list[1:-1][1].values]
+print("labels", len(labels))
+data = pd.DataFrame(y_list[0], labels, ["Actuals"])
+data['Predictions'] = y_hat_list[0][0]
+
+
+sns.lineplot(data=data, linewidth=1, markers=False, dashes=True)
+plt.xticks(fontsize=8, rotation=45)
+plt.show()
